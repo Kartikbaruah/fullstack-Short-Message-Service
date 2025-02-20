3382560f3df88a56be8b45f4148b89e479df48cb
Kartik Baruah <kbkartikbaruah1@gmail.com>
2025-02-20 16:22:11 +0530

    Initial commit

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import { useAuthStore } from '../store/useAuthStore';
+
+const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { login, isLoggingIn } = useAuthStore();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    login(formData);
+  };
+  
+  return (
+    <div>LoginPage</div>
+  )
+}
+
+export default LoginPage
